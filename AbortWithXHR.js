f2dcf27b7da330261574b28196190a2a78e3e6ef
Yves-Tabor <walvesoutis.gmail.com>
2026-02-08 19:24:12 +0200

    comm

---
+++ AbortWithXHR.js
@@ -0,0 +1,48 @@
+//1. abort method -> Aborting with SetTimeout and XHR
+
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://jsonplaceholder.typicode.com/comments");
+
+// const timeout = setTimeout(()=>{
+//     xhr.abort();
+//     console.log("Request Timeout !!");
+// }, 1000);
+
+// xhr.onreadystatechange = ()=>{
+//     if(xhr.readyState === 4){
+//         if(xhr.status === 200){
+//             console.log("Request successful : ", xhr.responseText);
+//             clearTimeout(timeout);
+//         }
+//     }
+// }
+// xhr.send();
+
+//2. abort event -> Aborting with SetTimeout and XHR
+
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://jsonplaceholder.typicode.com/comments");
+// xhr.addEventListener("abort", ()=>{
+//    console.log("Request aborted due to time !!"); 
+// });
+
+// const timeout = setTimeout(()=>{
+//     xhr.abort();
+// }, 2000);
+
+
+// xhr.onreadystatechange = () =>{
+//     if(xhr.status === 200){
+//         if(xhr.readyState === 4){
+//             setTimeout(()=>{
+//                 console.log("Request Successful : ", xhr.responseText)
+//             },1000);
+//         }else{
+//                 console.log("Request loading...")
+//         }
+//     }
+// }
+
+// xhr.send();
+
+//3. abort event -> Aborting with SetTimeout and XHR
