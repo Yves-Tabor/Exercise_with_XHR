5f81f0451fc556cd04ee86f30c87bd9b0b9c5048
Yves-Tabor <walvesoutis.gmail.com>
2026-02-08 14:22:27 +0200

    comm

---
+++ UseReadyStates.js
@@ -0,0 +1,18 @@
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://jsonplaceholder.typicode.com/comments", true);
+// xhr.onreadystatechange = ()=>{
+//     if(xhr.readyState === 2){
+//         console.log("Headers received");
+//     }
+//     else if(xhr.readyState === 3){
+//         setTimeout(()=>{
+//             console.log("Data is downloading !");
+//         }, 1000)
+//     }
+//     else if(xhr.readyState === 4){
+//         setTimeout(()=>{
+//             console.log("The process is done !");
+//         }, 2000)
+//     }
+// }
+// xhr.send();
